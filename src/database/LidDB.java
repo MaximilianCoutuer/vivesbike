227c040578f8d3d417809ee2759698441bd8b1b4
Gwij <gwij@live.be>
2017-11-30 11:57:52 +0100

    Initial commit

---
+++ src/database/LidDB.java
@@ -0,0 +1,35 @@
+package database;
+
+import databag.Lid;
+import java.util.ArrayList;
+
+public class LidDB implements InterfaceLidDB {
+
+    @Override
+    public void toevoegenLid(Lid lid)  {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void wijzigenLid(Lid lid)  {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void uitschrijvenLid(String rr)  {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Lid zoekLid(String rijksregisternummer)  {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Lid> zoekAlleLeden()  {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+  
+}
