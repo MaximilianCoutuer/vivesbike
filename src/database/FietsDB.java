a165c0413e3848f28cac7397a74de96f1f6dc77d
Maximilian Coutuer <Maximilian.Coutuer@gmail.com>
2017-12-14 11:32:15 +0100

    Add fietsDB.ToevoegenFiets()

--- src/database/FietsDB.java
+++ src/database/FietsDB.java
@@ -1,15 +1,47 @@
 package database;
 
 import databag.Fiets;
+import database.connect.ConnectionManager;
 import datatype.Status;
 import exception.DBException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class FietsDB implements InterfaceFietsDB {
 
     @Override
-    public Integer toevoegenFiets(Fiets fiets)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Integer toevoegenFiets(Fiets fiets) throws DBException {
+
+        if(fiets == null) {
+            //throw new ApplicationException();
+        }
+        
+        try(Connection conn = ConnectionManager.getConnection();)
+        {
+            try (PreparedStatement stmt = conn.prepareStatement(
+                    "insert into fiets (registratienummer"
+                            + ", status"
+                            + ", standplaats"
+                            + ", opmerkingen"
+                            + ") values(?,?,?,?)",
+                    Statement.NO_GENERATED_KEYS);)
+            {
+                stmt.setString(1, fiets.getRegistratienummer().toString());
+                stmt.setString(2, fiets.getStatus().name());
+                stmt.setString(3, fiets.getStandplaats().name());
+                stmt.setString(4, fiets.getOpmerking());
+                stmt.execute();
+            }
+            catch(SQLException e) {
+                throw new DBException("SQL-exception in toevoegenFiets - statement" + e);
+            }
+        }
+        catch(SQLException e) {
+            throw new DBException("SQL-exception in toevoegenFiets - connection" + e);
+        }
     }
 
     @Override
