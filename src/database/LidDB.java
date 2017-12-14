e4262e48008f06b22439e236004e5a4ec09b0d19
Gwij <gwij@live.be>
2017-12-14 11:34:02 +0100

    Update LidDB - toevoegenLid

--- src/database/LidDB.java
+++ src/database/LidDB.java
@@ -59,22 +59,22 @@ public class LidDB implements InterfaceLidDB {
 
     @Override
     public void wijzigenLid(Lid lid)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void uitschrijvenLid(String rr)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Lid zoekLid(String rijksregisternummer)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public ArrayList<Lid> zoekAlleLeden()  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
    
