cd257f7423cf1985e90f4babec53cbe5d053f589
Maximilian Coutuer <Maximilian.Coutuer@gmail.com>
2017-12-14 11:51:38 +0100

    Update FietsDB

--- src/database/FietsDB.java
+++ src/database/FietsDB.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class FietsDB implements InterfaceFietsDB {
 
+    // TODO: uitzoeken waarom return value vereist
     @Override
     public Integer toevoegenFiets(Fiets fiets) throws DBException {
 
@@ -42,6 +43,45 @@ public class FietsDB implements InterfaceFietsDB {
         catch(SQLException e) {
             throw new DBException("SQL-exception in toevoegenFiets - connection" + e);
         }
+        
+        /*try() {
+                String SQL = "SELECT USER_ID, USERNAME FROM DBUSER WHERE USER_ID = ?";
+
+		try {
+			dbConnection = getDBConnection();
+			preparedStatement = dbConnection.prepareStatement(SQL);
+			preparedStatement.setInt(1, 1001);
+
+			// execute select SQL stetement
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+
+				String userid = rs.getString("USER_ID");
+				String username = rs.getString("USERNAME");
+
+				System.out.println("userid : " + userid);
+				System.out.println("username : " + username);
+
+			}
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		} 
+        }*/
+        
         return 0; //TODO
     }
 
@@ -50,6 +90,12 @@ public class FietsDB implements InterfaceFietsDB {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Gegeven een registratienummer en opmerking, past de bijbehorende fiets aan
+     * @param regnr
+     * @param opmerking
+     * @throws DBException 
+     */
     @Override
     public void wijzigenOpmerkingFiets(Integer regnr, String opmerking) throws DBException  {
         
@@ -59,9 +105,10 @@ public class FietsDB implements InterfaceFietsDB {
         
         try(Connection conn = ConnectionManager.getConnection();)
         {
-            try (PreparedStatement stmt = conn.prepareStatement("UPDATE Messages SET opmerkingen = ? WHERE registratienummer = ?", Statement.NO_GENERATED_KEYS)) {
+            String SQL = "UPDATE Messages SET opmerkingen = ? WHERE registratienummer = ?";
+            try (PreparedStatement stmt = conn.prepareStatement(SQL, Statement.NO_GENERATED_KEYS)) {
                 stmt.setString(1, opmerking);
-                stmt.setString(2, regnr.toString());
+                stmt.setInt(2, regnr);
                 stmt.execute();
             }
             catch(SQLException e) {
