e4c2b5107ccbaa0dcac045905d7d42dc963d03a6
Maximilian Coutuer <Maximilian.Coutuer@gmail.com>
2017-12-21 11:06:52 +0100

    Save state.

--- src/database/FietsDB.java
+++ src/database/FietsDB.java
@@ -2,10 +2,13 @@ package database;
 
 import databag.Fiets;
 import database.connect.ConnectionManager;
+import datatype.Standplaats;
 import datatype.Status;
+import exception.ApplicationException;
 import exception.DBException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -89,46 +92,105 @@ public class FietsDB implements InterfaceFietsDB {
     public void wijzigenToestandFiets(Integer regnr, Status status)  {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     /**
-     * Gegeven een registratienummer en opmerking, past de bijbehorende fiets aan
+     * Gegeven een registratienummer en opmerking, pas de opmerking van de fiets aan die bij het registratienummer hoort.
      * @param regnr
      * @param opmerking
-     * @throws DBException 
+     * @throws DBException
+     * @throws ApplicationException 
+     * @author Maximilian Coutuer
      */
     @Override
-    public void wijzigenOpmerkingFiets(Integer regnr, String opmerking) throws DBException  {
+    public void wijzigenOpmerkingFiets(Integer regnr, String opmerking) throws DBException, ApplicationException {
         
-        if(regnr == null) {
-            //throw new ApplicationException();
-        }
-        
-        try(Connection conn = ConnectionManager.getConnection();)
+        validateRegnr(regnr);
+        try(Connection connection = ConnectionManager.getConnection();)
         {
             String SQL = "UPDATE Messages SET opmerkingen = ? WHERE registratienummer = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(SQL, Statement.NO_GENERATED_KEYS)) {
-                stmt.setString(1, opmerking);
-                stmt.setInt(2, regnr);
-                stmt.execute();
+            try (PreparedStatement statement = connection.prepareStatement(SQL, Statement.NO_GENERATED_KEYS)) {
+                statement.setString(1, opmerking);
+                statement.setInt(2, regnr);
+                statement.execute();
             }
             catch(SQLException e) {
-                throw new DBException("SQL-exception in toevoegenFiets - statement" + e);
+                throw new DBException("SQL-exception in toevoegenFiets - statement\n" + e + "\n");
             }
         }
         catch(SQLException e) {
-            throw new DBException("SQL-exception in toevoegenFiets - connection" + e);
+            throw new DBException("SQL-exception in toevoegenFiets - connection\n" + e + "\n");
         }
     }
 
+    /**
+     * Returns een fiets die overeenkomt met het gegeven registratienummer.
+     * @param regnr
+     * @return
+     * @throws DBException
+     * @throws ApplicationException 
+     * @author Maximilian Coutuer
+     */
     @Override
-    public Fiets zoekFiets(Integer regnr)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Fiets zoekFiets(Integer regnr) throws DBException, ApplicationException {
+        
+        validateRegnr(regnr);
+        try(Connection connection = ConnectionManager.getConnection();) {
+            String SQL = "SELECT * WHERE registratienummer = ?";
+            try (PreparedStatement statement = connection.prepareStatement(SQL, Statement.NO_GENERATED_KEYS)) {
+                statement.setInt(1, regnr);
+                ResultSet results = statement.executeQuery();
+                Fiets fiets = new Fiets();
+                fiets.setRegistratienummer(results.getInt("REGISTRATIENUMMER"));
+                fiets.setStatus(Status.valueOf(results.getString("STATUS")));
+                fiets.setStandplaats(Standplaats.valueOf(results.getString("STANDPLAATS")));
+                fiets.setOpmerking(results.getString("OPMERKINGEN"));
+                return fiets;
+            } catch(SQLException e) {
+                throw new DBException("SQL-exception in zoekFiets - statement\n" + e + "\n");
+            }
+        }
+        catch(SQLException e) {
+            throw new DBException("SQL-exception in zoekFiets - connection\n" + e + "\n");
+        }
     }
 
+    /**
+     * Returns een lijst van alle fietsen.
+     * @return
+     * @throws DBException
+     * @throws ApplicationException 
+     * @author Maximilian Coutuer
+     */
     @Override
-    public ArrayList<Fiets> zoekAlleFietsen()  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Fiets> zoekAlleFietsen() throws DBException, ApplicationException {
+        
+        try(Connection connection = ConnectionManager.getConnection();) {
+            String SQL = "SELECT *";
+            try (PreparedStatement statement = connection.prepareStatement(SQL, Statement.NO_GENERATED_KEYS)) {
+                ResultSet results = statement.executeQuery();
+                ArrayList<Fiets> fietsen = new ArrayList<>();
+                while (results.next()) {
+                    Fiets fiets = new Fiets();
+                    fiets.setRegistratienummer(results.getInt("REGISTRATIENUMMER"));
+                    fiets.setStatus(Status.valueOf(results.getString("STATUS")));
+                    fiets.setStandplaats(Standplaats.valueOf(results.getString("STANDPLAATS")));
+                    fiets.setOpmerking(results.getString("OPMERKINGEN"));
+                    fietsen.add(fiets);
+                }
+                return fietsen;
+            } catch(SQLException e) {
+                throw new DBException("SQL-exception in zoekAlleFietsen - statement\n" + e + "\n");
+            }
+        } catch(SQLException e) {
+            throw new DBException("SQL-exception in zoekAlleFietsen - connection\n" + e + "\n");
+        }
     }
 
+    
+    private void validateRegnr(Integer regnr) throws ApplicationException {
+        if(regnr == null) {
+            throw new ApplicationException("Registratienummer fiets is verplicht");
+        }
+    }
    
 }
